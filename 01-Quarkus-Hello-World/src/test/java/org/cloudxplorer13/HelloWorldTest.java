a88c0f1c6ea2e6eae5a28ca1eff931def39845d2
mukesh1327 <mugicoder13@gmail.com>
2025-09-14 12:54:15 +0530

    Added tests

---
+++ 01-Quarkus-Hello-World/src/test/java/org/cloudxplorer13/HelloWorldTest.java
@@ -0,0 +1,24 @@
+package org.cloudxplorer13;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+
+@QuarkusTest
+public class HelloWorldTest {
+
+    @Test
+    public void testRootEndpoint() {
+        given()
+          .when().get("/")
+          .then()
+             .statusCode(200)
+             .header("Content-Type", containsString("text/html"))
+             .body(containsString("<h1>"))
+             .body(containsString("<h2>App modernization</h2>"))
+             .body(containsString("<img"))
+             .body(containsString("alt='Image'"));
+    }
+}
